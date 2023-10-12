2e6bee79bd06128b0655d91564b10df105cb111c
KarolSU96 <>
2023-10-12 09:34:22 +0000

    add settings

--- codestar/settings.py
+++ codestar/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
-import os 
+import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['blog-django-bacon.heroku.com', 'localhost']
+ALLOWED_HOSTS = ['blog-django-bacon.herokuapp.com', 'localhost']
 
 
 # Application definition
