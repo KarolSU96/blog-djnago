7676fdff4af549f9d769a4a4cb74e2ef9b3d9d72
KarolSU96 <>
2023-10-12 10:04:18 +0000

    Add django project as allowed host

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['blog-django-bacon.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['blog-django-bacon-b2610180ba56.herokuapp.com', 'localhost']
 
 
 # Application definition
