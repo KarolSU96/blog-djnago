43f329452fd948d84361f4b62b6129c1fa0f2d7b
KarolSU96 <>
2023-10-12 11:44:46 +0000

    Add models and allowed hosts

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['blog-django-bacon-b2610180ba56.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = [
+    '8000-karolsu96-blog-djnago-wtcmhbz47k.us2.codeanyapp.com', 'localhost']
 
 
 # Application definition
